ca1db264853788c837567b863e738c2851c93b1d
Mebin Joseph <mebinmbn.joseph04@gmail.com>
2025-01-16 10:50:25 +0530

    updates in home page

--- frontend/src/pages/patient/Home.tsx
+++ frontend/src/pages/patient/Home.tsx
@@ -34,7 +34,7 @@ function Home() {
           <img
             src={doctor_img}
             alt=""
-            className="w-full md:w-72 my-5 md:my-0"
+            className="w-full  my-5 hidden md:block"
           />
           <div className="w-full md:w-3/5 mx-auto text-left p-4 md:p-10 mt-10 md:mt-0 flex flex-col md:flex-row justify-evenly">
             <p className="text-xl font-bold">Would you like to work with us?</p>
