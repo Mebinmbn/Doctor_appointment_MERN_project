2bb0680520f81a144dda2ffd2b82f72452f3aaf8
Mebin Joseph <mebinmbn.joseph04@gmail.com>
2025-01-16 11:37:25 +0530

    updating in payment page

--- frontend/src/pages/patient/PaymentPage.tsx
+++ frontend/src/pages/patient/PaymentPage.tsx
@@ -227,7 +227,7 @@ function PaymentPage() {
       <div className="payment_page_continer w-full md:w-[80vw] bg-gray-200 p-5 mt-5 h-screen mx-auto">
         <p className="font-extrabold text-3xl text-[#007E85] ">Payment</p>
         <div className="flex mt-5 w-full flex-wrap ">
-          <div className="w-full md:w-80 mr-5 border-2 p-3 rounded-lg border-[#007E85] text-right">
+          <div className="w-full md:w-[80%] mr-5 border-2 p-3 rounded-lg border-[#007E85] text-right">
             <div className="flex gap-10 items-center mt-5 border-2 border-green-800 rounded-lg p-5">
               <div>
                 <input
