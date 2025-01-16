ec24103dc2ea382addbfe2020700e356a565bb22
Mebin Joseph <mebinmbn.joseph04@gmail.com>
2025-01-16 14:03:39 +0530

    appointments page

--- frontend/src/pages/patient/Appointments.tsx
+++ frontend/src/pages/patient/Appointments.tsx
@@ -80,76 +80,78 @@ function Appointments() {
     setCurrentPage(pageNumber);
   };
   return (
-    <div className="flex items-center justify-center min-h-screen bg-[#007E85] gap-5">
+    <div className="md:flex items-center justify-center min-h-screen bg-[#007E85] gap-5">
       <PatientSideBar />
-      <div className="bg-white h-fit min-h-[98vh] w-[88vw] text-center p-4 rounded-l-[4rem] drop-shadow-xl border-[1px] border-[#007E85] ml-auto me-2">
+      <div className="bg-white h-fit min-h-[98vh] w-full md:w-[88vw] text-center p-4 md:rounded-l-[4rem] drop-shadow-xl border-[1px] border-[#007E85] ml-auto md:me-2">
         <PatientTopBar />
         <div className="flex justify-center items-center">
           <div className="w-full max-w-6xl mt-5 shadow-lg rounded-lg bg-[#007E85]">
             <h2 className="text-2xl font-bold mb-4 text-white p-4 border-b">
               Appointments
             </h2>
-            <table className="min-w-full bg-[#007E85] border">
-              <thead>
-                <tr>
-                  <th className="py-2 px-4 text-white border-b">Doctor</th>
-                  <th className="py-2 px-4 text-white border-b">Date</th>
-                  <th className="py-2 px-4 text-white border-b">Time</th>
-                  <th className="py-2 px-4 text-white border-b">Payment</th>
-                  <th className="py-2 px-4 text-white border-b">Status</th>
-                  <th className="py-2 px-4 text-white border-b">Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {currentAppointments.length > 0 ? (
-                  currentAppointments.map((appointment) => (
-                    <tr key={appointment._id}>
-                      <td className="py-2 px-4 text-white border-b">
-                        Dr. {appointment.doctorId?.firstName}
-                      </td>
-                      <td className="py-2 px-4 text-white border-b">
-                        {appointment.date.toString().slice(0, 10)}
-                      </td>
-                      <td className="py-2 px-4 text-white border-b">
-                        {appointment.time}
-                      </td>
-                      <td className="py-2 px-4 text-white border-b">
-                        {appointment.payment}
-                      </td>
-                      <td className="py-2 px-4 text-white border-b">
-                        {appointment.status}
-                      </td>
-                      <td className="py-2 px-4 text-white border-b">
-                        <div>
-                          <button
-                            className="bg-blue-500 rounded-xl px-2 py-1 border-[1px] mr-2"
-                            onClick={() => {
-                              handleView(appointment._id, appointment.status);
-                            }}
-                          >
-                            View
-                          </button>
-                          {appointment.status === "pending" && (
+            <div className="overflow-x-auto">
+              <table className="min-w-full bg-[#007E85] border border-collapse">
+                <thead>
+                  <tr>
+                    <th className="py-2 px-4 text-white border-b">Doctor</th>
+                    <th className="py-2 px-4 text-white border-b">Date</th>
+                    <th className="py-2 px-4 text-white border-b">Time</th>
+                    <th className="py-2 px-4 text-white border-b">Payment</th>
+                    <th className="py-2 px-4 text-white border-b">Status</th>
+                    <th className="py-2 px-4 text-white border-b">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {currentAppointments.length > 0 ? (
+                    currentAppointments.map((appointment) => (
+                      <tr key={appointment._id}>
+                        <td className="py-2 px-4 text-white border-b">
+                          Dr. {appointment.doctorId?.firstName}
+                        </td>
+                        <td className="py-2 px-4 text-white border-b">
+                          {appointment.date.toString().slice(0, 10)}
+                        </td>
+                        <td className="py-2 px-4 text-white border-b">
+                          {appointment.time}
+                        </td>
+                        <td className="py-2 px-4 text-white border-b">
+                          {appointment.payment}
+                        </td>
+                        <td className="py-2 px-4 text-white border-b">
+                          {appointment.status}
+                        </td>
+                        <td className="py-2 px-4 text-white border-b">
+                          <div>
                             <button
-                              className="bg-red-500 rounded-xl p-1 border-[1px] mr-2"
-                              onClick={() => handleCancel(appointment._id)}
+                              className="bg-blue-500 rounded-xl px-2 py-1 border-[1px] mr-2"
+                              onClick={() => {
+                                handleView(appointment._id, appointment.status);
+                              }}
                             >
-                              Cancel
+                              View
                             </button>
-                          )}
-                        </div>
+                            {appointment.status === "pending" && (
+                              <button
+                                className="bg-red-500 rounded-xl p-1 border-[1px] mr-2"
+                                onClick={() => handleCancel(appointment._id)}
+                              >
+                                Cancel
+                              </button>
+                            )}
+                          </div>
+                        </td>
+                      </tr>
+                    ))
+                  ) : (
+                    <tr>
+                      <td className="py-2 px-4 text-white border-b" colSpan={6}>
+                        No appointments found
                       </td>
                     </tr>
-                  ))
-                ) : (
-                  <tr>
-                    <td className="py-2 px-4 text-white border-b" colSpan={6}>
-                      No appointments found
-                    </td>
-                  </tr>
-                )}
-              </tbody>
-            </table>
+                  )}
+                </tbody>
+              </table>
+            </div>
             <div className="flex justify-center mt-4">
               {Array.from({ length: totalPages }, (_, i) => i + 1).map(
                 (page) => (
